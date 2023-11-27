3bb57813f53a24d4727cf28b098f6f0890be947e
asamba <abdoulaye.samba@awaconsulting.com>
2023-11-27 18:00:18 +0000

    first commit

---
+++ src/main/java/com/meddjamm/sn/MedDjammApplication.java
@@ -0,0 +1,13 @@
+package com.meddjamm.sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedDjammApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedDjammApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meddjamm/sn/MedDjammApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meddjamm.sn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedDjammApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
