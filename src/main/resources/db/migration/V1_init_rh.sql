0b02604f7793492d308190860938e176875b2b54
tairou <thirdiallo@gmail.com>
2024-04-28 11:33:06 +0000

    delete commentaire

--- src/main/resources/db/migration/V1_init_rh.sql
+++ src/main/resources/db/migration/V1_init_rh.sql
@@ -28,14 +28,14 @@ Values (1,'AA','Artésunate/Amodiaquine ','2024-03-08 00:21:34.27', 1),
 INSERT INTO ged_type_document (id, code, libelle, actif)
 VALUES (1, 'TYPE_PHOTO_PAT', 'Photo du patient', 1),
        (2, 'TYPE_PHOTO_PROF', 'Photo profile pour un agent',1),
-       (3, 'TYPE_EXAM_CONS_BIO', 'Consultation examen biologie',1),
-       (4, 'TYPE_EXAM_CONS_IMMUNO', 'Consultation examen immunologie',1),
-       (5, 'TYPE_EXAM_CONS_IMG', 'Consultation examen imagerie',1),
-       (6, 'TYPE_EXAM_CONS_ANA', 'Consultation examen',1),
-       (7, 'TYPE_EXAM_BIO_COMP', 'Examen complementaire biologie',1),
-       (8, 'TYPE_EXAM_IMMUNO_COMP', 'Examen complementaire immunologie',1),
-       (9, 'TYPE_EXAM_IMG_COMP', 'Examen complementaire imagerie',1),
-       (10, 'TYPE_EXAM_ANA_COMP', 'Examen complementaire anatologie',1);
+       (3, 'TYPE_EXAM_CONS_BIO', 'Consultation biologie',1),
+       (4, 'TYPE_EXAM_CONS_IMMUNO', 'Consultation immunologie',1),
+       (5, 'TYPE_EXAM_CONS_IMG', 'Consultation imagerie',1),
+       (6, 'TYPE_EXAM_CONS_ANA', 'Consultation Anato',1),
+       (7, 'TYPE_EXAM_BIO_COMP', 'Examen biologie',1),
+       (8, 'TYPE_EXAM_IMMUNO_COMP', 'Examen immunologie',1),
+       (9, 'TYPE_EXAM_IMG_COMP', 'Examen imagerie',1),
+       (10, 'TYPE_EXAM_ANA_COMP', 'Examen Anato',1);
        (11, 'TYPE_PROTOCOLE_MEDIC', 'Protocole médical',1);
 
 
