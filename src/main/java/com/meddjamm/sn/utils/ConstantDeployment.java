d0e10916aa0f007b8feee45486b6c8b50f26a72a
tairou <thirdiallo@gmail.com>
2024-05-05 21:06:48 +0000

    remove comment

--- src/main/java/com/meddjamm/sn/utils/ConstantDeployment.java
+++ src/main/java/com/meddjamm/sn/utils/ConstantDeployment.java
@@ -3,10 +3,7 @@ package com.meddjamm.sn.utils;
 public final class ConstantDeployment {
 
     //   private static final String DEST_ENV = "/opt/tomcat/apache-tomcat-9.0.78/webapps/ged_epme/";
-
-    //  private static final String DEST_ENV = "/dossier_medical/";
-
-
+    
     private static final String DEST_ENV = "src/main/resources/static/webapp/document/";
 
     public static final String ged_dmi = DEST_ENV + "multi_ged/dmi";
