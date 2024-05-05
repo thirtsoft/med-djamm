3591e7320ef3d7eeb6d05110a42723114c04ab12
tairou <thirdiallo@gmail.com>
2024-05-05 22:40:29 +0000

    pointe files to heroku

--- src/main/java/com/meddjamm/sn/utils/ConstantDeployment.java
+++ src/main/java/com/meddjamm/sn/utils/ConstantDeployment.java
@@ -3,8 +3,10 @@ package com.meddjamm.sn.utils;
 public final class ConstantDeployment {
 
     //   private static final String DEST_ENV = "/opt/tomcat/apache-tomcat-9.0.78/webapps/ged_epme/";
-    
-    private static final String DEST_ENV = "src/main/resources/static/webapp/document/";
+
+//    private static final String DEST_ENV = "src/main/resources/static/webapp/document/";
+
+    private static final String DEST_ENV = "https://github.com/thirtsoft/med-djamm/tree/prodServeur_22_04_2024/src/main/resources/static/webapp/document/";
 
     public static final String ged_dmi = DEST_ENV + "multi_ged/dmi";
 
